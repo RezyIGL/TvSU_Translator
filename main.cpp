314d0265c38bd8eb5331acb804049282b6273625
Rezy <onlyrezystor@mail.ru>
2024-04-03 19:16:55 +0300

    little main fix

--- main.cpp
+++ main.cpp
@@ -4,20 +4,6 @@
 #include "ll.h"
 
 int main() {
-//	std::vector<std::string> inputs = {
-//		"int main() {int a = 3; char c = '5'; a = 2 + c; in b; out 13 + 2; return 4;}",
-//	};
-//
-//	for (const auto &inp : inputs) {
-//		std::stringstream ss(inp);
-//		LL ll = LL(ss);
-//		using namespace std;
-//		cout << "============================================================================================" << endl;
-//		cout << "Expression: " << inp << endl;
-//		cout << "Result: " << ll.validate() << endl;
-//		cout << "============================================================================================\n" << endl;
-//	}
-
 	std::fstream streamline(R"(E:\CLionProjects\TvSU_Translator\miniC-code.txt)");
 	LL ll(streamline);
 	std::cout << ll.validate() << std::endl;
