2a216880f20fd0a9c1a354775969669587ec81a4
Rezy <onlyrezystor@mail.ru>
2024-04-10 13:16:51 +0300

    net

--- main.cpp
+++ main.cpp
@@ -1,8 +1,12 @@
 #include "ll.h"
+#include <fstream>
 
 int main() {
  // TODO: Pass inputFile name to LL constructor
-	LL ll();
+    std::string output = R"(E:\CLionProjects\TvSU_Translator\output.txt)";
+    std::fstream input(R"(E:\CLionProjects\TvSU_Translator\miniC-code.txt)");
+
+	LL ll(input, output);
 	ll.validate();
 
 	return 0;
