516b9dd087d83e783f8e70d0f56dc94fa2de99cd
Rezy <onlyrezystor@mail.ru>
2024-04-28 10:08:36 +0300

    Test file change

--- files/Result.asm
+++ files/Result.asm
@@ -4,16 +4,18 @@ int_0_n: db 11
 int_0_s1: db 0
 int_0_s2: db 0
 int_0_s3: db 0
+int_0_n2: db 1
 int_for_0_0_i: db 0
 int_for_0_0_$T1: db 0
 int_for_0_0_$T2: db 0
 int_for_for_0_0_1_j: db 0
 int_for_for_0_0_1_$T3: db 0
 int_for_for_0_0_1_$T4: db 0
+int_for_for_0_0_1_$T5: db 0
 int_for_0_2_i: db 0
-int_for_0_2_$T5: db 0
 int_for_0_2_$T6: db 0
 int_for_0_2_$T7: db 0
+int_for_0_2_$T8: db 0
 
 START:
 JMP main
@@ -32,6 +34,9 @@ STA int_0_s2
 MVI A, 0
 STA int_0_s3
 
+MVI A, 1
+STA int_0_n2
+
 MVI A, 0
 STA int_for_0_0_i
 
@@ -40,7 +45,7 @@ L1:
 MVI A, 1
 STA int_for_0_0_$T1
 
-LDA int_0_n
+LDA int_0_n2
 MOV B, A
 LDA int_for_0_0_i
 CMP B
@@ -83,7 +88,7 @@ L6:
 MVI A, 1
 STA int_for_for_0_0_1_$T3
 
-LDA int_0_n
+LDA int_0_n2
 MOV B, A
 LDA int_for_for_0_0_1_j
 CMP B
@@ -118,6 +123,15 @@ JMP L6
 
 L8:
 
+LDA int_for_for_0_0_1_j
+MOV B, A
+LDA int_0_s2
+ADD B
+STA int_for_for_0_0_1_$T5
+
+LDA int_for_for_0_0_1_$T5
+STA int_0_s2
+
 JMP L7
 
 L9:
@@ -132,7 +146,7 @@ STA int_for_0_2_i
 L11:
 
 MVI A, 1
-STA int_for_0_2_$T5
+STA int_for_0_2_$T6
 
 LDA int_0_n
 MOV B, A
@@ -142,13 +156,13 @@ JM L15
 JZ L15
 
 MVI A, 0
-STA int_for_0_2_$T5
+STA int_for_0_2_$T6
 
 L15:
 
 MVI A, 0
 MOV B, A
-LDA int_for_0_2_$T5
+LDA int_for_0_2_$T6
 CMP B
 JZ L14
 
@@ -160,9 +174,9 @@ MVI A, 1
 MOV B, A
 LDA int_for_0_2_i
 ADD B
-STA int_for_0_2_$T6
+STA int_for_0_2_$T7
 
-LDA int_for_0_2_$T6
+LDA int_for_0_2_$T7
 STA int_for_0_2_i
 
 JMP L11
@@ -173,9 +187,9 @@ LDA int_for_0_2_i
 MOV B, A
 LDA int_0_s1
 ADD B
-STA int_for_0_2_$T7
+STA int_for_0_2_$T8
 
-LDA int_for_0_2_$T7
+LDA int_for_0_2_$T8
 STA int_0_s1
 
 JMP L12
