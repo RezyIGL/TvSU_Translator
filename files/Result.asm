e7b522df201a958eb788fa54bbb8fcc3b0515e56
Rezy <onlyrezystor@gmail.com>
2024-05-02 08:45:08 +0300

    changed test code

--- files/Result.asm
+++ files/Result.asm
@@ -1,40 +1,46 @@
 JMP START
 
-int_0_a: db 0
-int_0_$T1: db 0
-int_0_$T2: db 0
+int_0_n: db 10
+int_0_s: db 0
+int_1_i: db 0
+int_1_$T1: db 0
+int_1_$T2: db 0
+int_1_$T3: db 0
 
 START:
 JMP main
 
 main:
 
+MVI A, 10
+STA int_0_n
+
 MVI A, 0
-STA int_0_a
+STA int_0_s
 
-LDA int_0_a
-STA int_0_$T1
-
-MVI A, 1
-MOV B, A
-LDA int_0_a
-ADD B
-STA int_0_a
-
-LDA int_0_a
-STA int_0_$T2
-
-MVI A, 1
-MOV B, A
-LDA int_0_a
-SUB B
-STA int_0_a
+MVI A, 0
+STA int_1_i
 
 L1:
 
+MVI A, 1
+STA int_1_$T1
+
+LDA int_0_n
+MOV B, A
+LDA int_1_i
+CMP B
+JM L5
+JZ L5
+
+MVI A, 0
+STA int_1_$T1
+
+L5:
+
 MVI A, 0
 MOV B, A
-MVI A, 
+LDA int_1_$T1
 CMP B
 JZ L4
 
@@ -42,13 +48,34 @@ JMP L3
 
 L2:
 
+LDA int_1_i
+STA int_1_$T2
+
+MVI A, 1
+MOV B, A
+LDA int_1_i
+ADD B
+STA int_1_i
+
 JMP L1
 
 L3:
 
+LDA int_1_i
+MOV B, A
+LDA int_0_s
+ADD B
+STA int_1_$T3
+
+LDA int_1_$T3
+STA int_0_s
+
 JMP L2
 
 L4:
+
+LDA int_0_s
+OUT 1
 
 ; RET IS NOT IMPLEMENTED
 
